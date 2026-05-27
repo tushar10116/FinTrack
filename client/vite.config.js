d2fdb5149f0d0a6a349a115f08f6f7bba6703d58
tushar10116 <37633490+tushar10116@users.noreply.github.com>
2026-05-27 10:00:10 +0530

    Update API proxy URL in Vite configuration

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/api': 'https://fintrack-ph48.onrender.com'
+      '/api': 'https://fintrack-ph48.onrender.com/api/'
     }
   }
 });
