e76f399aca3dfcc2dbefaa6e1c12ff9632439b85
Nandini Jadhav <tushar10116@gmail.com>
2026-05-27 14:39:28 +0530

    api base changed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/',
+  baseURL: 'https://fintrack-ph48.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
