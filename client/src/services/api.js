e125ce096287eb8ce3aa5cc4a27bfee5748fb8ae
Nandini Jadhav <tushar10116@gmail.com>
2026-05-29 14:02:04 +0530

    api updated

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: 'https://fintrack-ph48.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
